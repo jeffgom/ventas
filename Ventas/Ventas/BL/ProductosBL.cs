85f3cd160070bab3618d96394f73842a024b4661
jeffr <jeffr@DESKTOP-BRUJP0S>
2019-07-31 11:26:36 -0600

    Diseñando el fromulario de productos y clientes

---
+++ Ventas/Ventas/BL/ProductosBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas.Modelos;
+
+namespace Ventas.BL
+{
+    public class ProductosBL
+    {
+        public List<Productos> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            ListadeProductos = new List<Productos>();
+            CrearDatosdePrueba();
+        }
+
+        private void CrearDatosdePrueba()
+        {
+            var Cliente1 = new Clientes(1, "Alexander");
+            var Cliente2 = new Clientes(2, "Keneth");
+
+            var Producto1 = new Productos(1, "Computadora Dell", 15350, Cliente1);
+            var Producto2 = new Productos(2, "PlayStation", 8500, Cliente2);
+            var Producto3 = new Productos(3, "Mando de Video Juego", 1200, Cliente1);
+
+            ListadeProductos.Add(Producto1);
+            ListadeProductos.Add(Producto3);
+            ListadeProductos.Add(Producto2);
+        }
+    }
+}
---
+++ Ventas/Ventas/Form3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ventas.BL;
+
+namespace Ventas
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            var productosBL = new ProductosBL();
+            listadeProductosBindingSource.DataSource = productosBL.ListadeProductos;
+
+            var clientesBL = new ClientesBL();
+            listadeClientesBindingSource.DataSource = clientesBL.ListadeClientes;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
